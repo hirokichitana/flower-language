006be8cb88171d549db85443cfd797733196a772
hirokichitana <94688508+hirokichitana@users.noreply.github.com>
2022-05-21 23:45:04 +0900

    バリデーションの記述

--- app/models/flower.rb
+++ app/models/flower.rb
@@ -1,4 +1,6 @@
 class Flower < ApplicationRecord
   belongs_to :taist
   belongs_to :user
+
+  validates :name, presence: true
 end
--- app/models/type.rb
+++ app/models/type.rb
@@ -1,3 +1,8 @@
 class Type < ApplicationRecord
   has_many :flowers
+
+  validates :gorgeous, presence: true
+  validates :simple, presence: true
+  validates :formal, presence: true
+  validates :casual, presence: true
 end
