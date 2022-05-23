92c6617d620c803863a6bc358dd8307e17cefd53
hirokichitana <94688508+hirokichitana@users.noreply.github.com>
2022-05-23 20:21:10 +0900

    flowerマイグレーションの編集

--- db/migrate/20220517101910_create_flowers.rb
+++ db/migrate/20220517101910_create_flowers.rb
@@ -1,7 +1,8 @@
 class CreateFlowers < ActiveRecord::Migration[6.0]
   def change
     create_table :flowers do |t|
-      t.string     :name,  null: false
+      t.string  :flower_name,  null: false
+      t.string  :explanation,  null: false
       t.integer :type_id
       t.integer :user_id
       t.timestamps
